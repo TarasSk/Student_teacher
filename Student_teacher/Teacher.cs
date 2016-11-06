10fe6d45e0af0012cf34fd91c1a850125b2750a9
Taras <Taras@DESKTOP-0UHKL5J>
2016-11-06 21:56:22 +0200

    Add Teacher Class

---
+++ Student_teacher/Teacher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_teacher
+{
+    class Teacher
+    {
+        public Teacher(){ }
+        private Int32 _id;
+        private String _name;
+        private String _surname;
+        private List<Student> _students;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+            set { _surname = value; }
+        }
+
+        public List<Student> Students
+        {
+            get { return _students; }
+            set { _students = value; }
+        }
+    }
+}
