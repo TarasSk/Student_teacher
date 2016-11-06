93f0ff0f73a06927a3b20510335012cd091e443a
Taras <Taras@DESKTOP-0UHKL5J>
2016-11-06 21:49:53 +0200

    Add Student Class

---
+++ Student_teacher/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_teacher
+{
+    class Student
+    {
+        public Student()
+        {
+        }
+        public Int32 Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public String Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public String Surname
+        {
+            get { return _surname; }    
+            set { _surname = value; }
+        }
+
+        private Int32 _id;
+        private String _name;
+        private String _surname;
+    }
+}
